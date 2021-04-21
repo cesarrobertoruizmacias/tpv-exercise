5354e7386955e28d68c6e155a2e2266d57c763a1
cesarrobertoruizmacias <78805918+cesarrobertoruizmacias@users.noreply.github.com>
2021-04-21 11:25:57 -0500

    Delete src/oop/inheritance/core directory

--- src/oop/inheritance/core/TPVDisplay.java
+++
@@ -1,7 +0,0 @@
-package oop.inheritance.core;
-
-public interface TPVDisplay {
-    void print(int x, String message);
-    void lineFeed();
-    public void showMessage(int x, int y, String message);
-}
